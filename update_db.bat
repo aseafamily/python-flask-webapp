93ffbe6271f0518ca43ebc6ca94f34677744a62c
Alex Ma <alexmajin@hotmail.com>
2024-03-20 21:33:03 -0700

    backup db

--- update_db.bat
+++ update_db.bat
@@ -1,11 +1,31 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 
 rem Set the URL from which to download the file
 set "download_url=https://bluehousemall.azurewebsites.net/serve/db"
 
 rem Set the local file name to replace
 set "local_file=test.db"
+set "backup_folder=.\backup_db"
+
+rem Check if the backup folder exists, if not, create it
+if not exist "%backup_folder%" (
+    mkdir "%backup_folder%"
+)
+
+rem Get the current date in yyyyMMdd format using PowerShell
+for /f %%a in ('powershell -Command "Get-Date -Format 'yyyyMMdd'"') do set "current_date=%%a"
+
+rem Set backup file with yyyyMMdd format
+set "backup_file=%backup_folder%\%local_file%.!current_date!"
+
+rem Check if there are more than five backup copies, delete the oldest one
+powershell -Command "Get-ChildItem '%backup_folder%\%local_file%.*' | Sort-Object CreationTime -Descending | Select-Object -Skip 5 | Remove-Item -Force"
+
+rem Backup the existing file
+if exist "%local_file%" (
+    move /Y "%local_file%" "%backup_file%" >nul
+)
 
 rem Download the file from the URL
 echo Downloading file from %download_url%
