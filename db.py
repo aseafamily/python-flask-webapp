b05c7d25332018759069e20dcbbc55740fc83b1f
Alex Ma <alexmajin@hotmail.com>
2024-05-19 11:09:53 -0700

    db setting change

--- db.py
+++ db.py
@@ -1,5 +1,20 @@
 from flask_sqlalchemy import SQLAlchemy
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from a .env file if present
+load_dotenv()
+
+# Get the database credentials from environment variables
+username = os.getenv('DB_USERNAME')
+password = os.getenv('DB_PASSWORD')
+
+if username and password:
+    print("Database credentials loaded successfully.")
+else:
+    print("Failed to load database credentials.")
+
 
 db = SQLAlchemy()
 
-db_uri = 'mssql+pyodbc://azureuser:T%nt0wn1@bhmtest.database.windows.net/myTest?driver=ODBC+Driver+17+for+SQL+Server&connect_timeout=30'
+db_uri = f'mssql+pyodbc://{username}:{password}@bhmtest.database.windows.net/myTest?driver=ODBC+Driver+17+for+SQL+Server&connect_timeout=30'
