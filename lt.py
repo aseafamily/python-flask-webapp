6a9906201809a6b1c8dd361a4b01329dc436d533
Alex Ma <alexmajin@hotmail.com>
2024-08-01 22:29:13 -0700

    styles for lt

--- lt.py
+++ lt.py
@@ -50,22 +50,44 @@ def submit_log(log_type):
         return redirect(url_for('lt.log_form', log_type=log_type))
     return "Log type not found", 404
 
-@lt_bp.route('/lt/<log_type>/update/<int:log_id>', methods=['POST'])
+@lt_bp.route('/lt/update/<log_type>/<int:log_id>', methods=['GET'])
+def update_form(log_type, log_id):
+    if log_type in config['log_types']:
+        log_config = config['log_types'][log_type]
+        filename = f"{log_type}_logs.json"
+        try:
+            with open(filename, 'r') as f:
+                logs = [json.loads(line) for line in f]
+        except FileNotFoundError:
+            logs = []
+
+        log = next((entry for entry in logs if int(entry.get('id', 0)) == log_id), None)
+        if log:
+            return render_template('lt_update_form.html', log_type=log_type, log_config=log_config, log=log, log_id=log_id)
+        return "Log entry not found", 404
+    return "Log type not found", 404
+
+@lt_bp.route('/lt/update/<log_type>/<int:log_id>', methods=['POST'])
 def update_log(log_type, log_id):
     if log_type in config['log_types']:
-        updated_log_data = request.form.to_dict()
-        logs = read_logs(log_type)
+        log_data = request.form.to_dict()
+        log_data["id"] = log_id
+        filename = f"{log_type}_logs.json"
+        try:
+            with open(filename, 'r') as f:
+                logs = [json.loads(line) for line in f]
+        except FileNotFoundError:
+            logs = []
 
-        for log in logs:
-            if log.get('id') == log_id:
-                log.update(updated_log_data)
-                break
-
-        write_logs(log_type, logs)
+        logs = [log_data if int(log.get('id', 0)) == log_id else log for log in logs]
+        with open(filename, 'w') as f:
+            for log in logs:
+                f.write(json.dumps(log) + '\n')
         return redirect(url_for('lt.log_form', log_type=log_type))
     return "Log type not found", 404
 
-@lt_bp.route('/lt/<log_type>/delete/<int:log_id>', methods=['POST'])
+
+@lt_bp.route('/lt/<log_type>/delete/<int:log_id>', methods=['GET'])
 def delete_log(log_type, log_id):
     if log_type in config['log_types']:
         logs = read_logs(log_type)
