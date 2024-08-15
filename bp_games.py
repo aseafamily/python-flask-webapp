62b9c3d268615b30e3e5ece0d6c4d88e1c2d7bdb
Alex Ma <alexmajin@hotmail.com>
2024-08-14 17:18:48 -0700

    games

--- bp_games.py
+++ bp_games.py
@@ -1,8 +1,28 @@
-from flask import Blueprint
-from flask import render_template, request, redirect
+from flask import Blueprint, render_template
 
 games_bp = Blueprint('games', __name__)
+
+@games_bp.route('/games/tictactoe')
+def tictactoe():
+    return render_template('games_tictactoe.html')
+
+@games_bp.route('/games/pong')
+def pong():
+    return render_template('games_pong.html')
+
+@games_bp.route('/games/whack-a-mole')
+def whack_a_mole():
+    return render_template('games_whack_a_mole.html')
+
+@games_bp.route('/games/minecraft')
+def minecraft():
+    return render_template('games_minecraft.html')
+
 
 @games_bp.route('/games/test')
 def games_index():
     return render_template('games_test.html')
+
+@games_bp.route('/games/guess-number')
+def guess_number():
+    return render_template('games_guess_number.html')
